72bed613d2d51daf06b4473a5ba78531b8246d92
JordanJimenezVA <jordan.jimenez.valenzuela@gmail.com>
2024-09-25 23:12:43 -0300

    fix

--- index.js
+++ index.js
@@ -124,12 +124,12 @@ app.post('/Login', async (req, res) => {
 
             if (isMatch) {
                 const rut = user.RUTU;
-                const secretKey = process.env.JWT_SECRET_KEY || 'default-secret-key';
+                const secretKey = process.env.JWT_SECRET_KEY;
                 const token = jwt.sign({ rut }, secretKey, { expiresIn: '1d' });
 
                 res.cookie('token', token, {
-                    httpOnly: false,
-                    secure: false,  // Railway utiliza HTTPS, por lo tanto, secure debe estar en true
+                    httpOnly: true,
+                    secure: true,  // Railway utiliza HTTPS, por lo tanto, secure debe estar en true
                     domain: '.up.railway.app',  // El dominio base, compartido entre el frontend y el backend
                     sameSite: 'None',  // Necesario para habilitar cookies entre diferentes dominios/subdominios
                     maxAge: 24 * 60 * 60 * 1000  // 1 día
