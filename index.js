319f7b8ee033873c78604b15a2bedd070293c9dc
JordanJV <97534634+JordanJimenezVA@users.noreply.github.com>
2025-12-05 20:29:57 -0300

    Update cookie settings for security enhancements

--- index.js
+++ index.js
@@ -141,9 +141,9 @@ app.post("/Login", async (req, res) => {
             : "Instalación no encontrada";
 
         res.cookie("token", token, {
-          httpOnly: false,
-          secure: false,
-          //domain: ".up.railway.app",
+          httpOnly: true,
+          secure: true,
+          domain: ".up.railway.app",
           sameSite: "None",
           maxAge: 24 * 60 * 60 * 1000, // 1 día
         });
